0ee4de1adc942c57cafe8bc550326735434098ff
Juraj Pejnovic <peggia@Fives-MacBook-Pro.local>
2018-07-04 18:40:37 +0200

    Add Fetch

--- src/App.js
+++ src/App.js
@@ -9,17 +9,32 @@ export default class App extends Component {
     this.state = {
       isNewsSelected: false
     };
+
+
   }
 
   render() {
     return (
       <View>
         {this.state.isNewsSelected && <NewsDetails />}
-        {!this.state.isNewsSelected && <NewsList />}
+        {!this.state.isNewsSelected && <NewsList artices={this.state.artices}/>}
       </View>
     );
   }
+
+  componentDidMount(){
+    fetch('http://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&api-key=718c5c8e6f1e4af3afcc4611eb5d690c')
+    .then((response) => response.json()).then((responseJson) => this.parseArticles(responseJson));
+  }
+
+  parseArticles(articleJson){
+    let parsedArticles;
+
+    this.setState({articles: parsedArticles});
+  }
 }
+
+
 
 const styles = StyleSheet.create({
 
