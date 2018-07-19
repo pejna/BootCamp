096e7bf9cbaaa3e935f6376445b584723778c5a1
Juraj Pejnovic <peggia@Peggias-computer.local>
2018-07-19 14:39:18 +0200

    Fix Modal Screen errors

--- src/screens/modal/PlaceholderModalScreen.js
+++ src/screens/modal/PlaceholderModalScreen.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default (PlaceholderModalScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 30 }}>Coming soon!</Text>
-      <Button onPress={() => navigation.goBack()} title="Dismiss" />
-    </View>
-  );
-});
+export default class PlaceholderModalScreen extends Component {
+  onPress() {
+    const { navigation } = this.props;
+    navigation.goBack();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Coming soon!</Text>
+        <Button onPress={this.onPress} title="Dismiss" />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
   },
 });
